6176e2ae4f29d794166cf16a10059961adfc00af
James K. Glasbrenner <jglasbr2@gmu.edu>
2019-07-03 08:24:56 -0400

    An entity is just a simple id

---
+++ spyns/lib/entities.hpp
@@ -0,0 +1,11 @@
+#ifndef SPYNS_LIB_ENTITIES_HPP
+#define SPYNS_LIB_ENTITIES_HPP
+
+#include <cstdint>
+
+namespace entities {
+struct Entity {
+  uint64_t id;
+};
+}   // namespace entities
+#endif   // SPYNS_LIB_ENTITIES_HPP
